a5cc9f78c6142b74a45545fd3661fd5af1244929
Jean-philippe HOAREAU MEAVILLA <jeahoare@e3r2p8.42.fr>
2017-06-19 07:56:28 +0200

    Norme : allowed globals' structs with exemples

--- main.c
+++ main.c
@@ -27,12 +27,12 @@ int main(int argc, char **argv)
 	ft_main_struct()->ac = argc;	// Assignation des valeurs;
 	ft_main_struct()->av = argv;	// //
 
-	printf("\nargc = %d, ac = %d\n\n", argc, ft_main_struct()->ac);			// Verification de l attribution de données aux valeurs de la structure;
-	printf("name_exe = %s\n", ft_main_struct()->av[0]);	// //
-	while (ft_main_struct()->av[i])							// //
-	{														// //
-		printf("arg %d : %s\n", i, ft_main_struct()->av[i]);// //
-		i++;												// //
-	}														// //
+	printf("\nargc = %d, ac = %d\n\n", argc, ft_main_struct()->ac);	// Verification de l attribution de données aux valeurs de la structure;
+	printf("name_exe = %s\n", ft_main_struct()->av[0]);				// //
+	while (ft_main_struct()->av[i])									// //
+	{																// //
+		printf("arg %d : %s\n", i, ft_main_struct()->av[i]);		// //
+		i++;														// //
+	}																// //
 	return (0);
 }
